7801018bae0a89d36e3a591cf664d8aa9e9fa69b
Tamal H <tamal.nh@gmail.com>
2019-08-18 21:19:39 +0600

    updated checkout info

---
+++ src/components/login/index.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+import './login.scss' 
+
+const Login = (props) => {
+
+    const clickHandler = () =>{
+        props.handleClick() 
+    }
+    console.log(props)
+    return ( 
+        <div className="__login">
+            <div>
+                <button type="submit" onClick={() => clickHandler()}>CLick to login</button>
+
+                <p>this app use all the dummy data. Please click here to login</p>
+            </div>
+        </div>
+     );
+}
+ 
+export default Login;
--- src/components/pages/checkout.js
+++ src/components/pages/checkout.js
@@ -1,5 +1,10 @@
 import React, { Component } from 'react';
-import {Link} from 'react-router-dom'
+import {Link} from 'react-router-dom';
+import {connect} from 'react-redux';
+
+import Login from '../login';
+
+import './checkout.scss'
 
 class Checkout extends Component {
     constructor(props) {
@@ -11,35 +16,93 @@ class Checkout extends Component {
             alternetPhone: '',
             city: '',
             zipCode: '',
-            address: ''
+            address: '',
+
+            isLoggedIn: true
          }
 
+         this.loginHandler = this.loginHandler.bind(this);
+
+
     }
+
+
+    componentDidMount(){
+        this.setState({
+            isLoggedIn: false
+        })
+
+
+    }
+
+    loginHandler(){ 
+        this.setState({
+            isLoggedIn: true,
+            name: 'Bruce J Crenshaw',
+            email: 'brucejcrenshaw@gmail.com',
+            phone: '617-439-8453',
+            alternetPhone: '857-207-2287',
+            city: 'Boston',
+            zipCode: '02107',
+            address: '3186  Hinkle Lake Road',
+        })  
+    }
+
+
     render() { 
+        console.log(this.props.cart)
+        const {cart} = this.props.cart;
         return ( 
             <div className="chekout__wrapper">
+                {!this.state.isLoggedIn && (<Login handleClick={this.loginHandler} />)}
+
                 <div className="checkout__form">
                     <div className="form__inline">
-                        <input type="text" placeholder="name..." name="name" />
+                        <input 
+                            type="text" 
+                            placeholder="*name..." 
+                            name="name" 
+                            defaultValue={this.state.name}/>
                     </div>
                     <div className="form__inline">
-                        <input type="email" placeholder="email..." name="email"/>  
+                        <input 
+                            type="email"
+                            placeholder="*email..." 
+                            name="email"
+                            defaultValue={this.state.email}/>  
                     </div>
                     <div className="form__inline">
-                        <input type="text" placeholder="phone number..." name="phone"/>
-                        <input type="text" placeholder="alternet phone number..." name="alternetPhone"/>
+                        <input 
+                            type="text" 
+                            placeholder="*phone number..." 
+                            name="phone"
+                            defaultValue={this.state.phone}/>
+                        <input 
+                            type="text" 
+                            placeholder="alternet phone number..." 
+                            name="alternetPhone"
+                            defaultValue={this.state.alternetPhone}/>
                     </div>
                     <div className="form__inline">
-                        <input type="text" placeholder="city..." name="city"/> 
-                        <input type="text" placeholder="zip code..." name="zipCode"/> 
-                    </div>
-                    <div className="form__inline">
-                        <input type="text" placeholder="cityr..." name="city"/> 
-                        <input type="text" placeholder="zip code..." name="zipCode"/> 
-                    </div>
+                        <input 
+                            type="text" 
+                            placeholder="*city..." 
+                            name="city"
+                            defaultValue={this.state.city}/> 
+                        <input 
+                            type="text" 
+                            placeholder="*zip code..." 
+                            name="zipCode"
+                            defaultValue={this.state.zipCode}/> 
+                    </div> 
                     
                     <div className="form__inline">
-                        <textarea name="address" placeholder="address"></textarea>  
+                        <textarea 
+                            name="address" 
+                            placeholder="*address" 
+                            value={this.state.address} 
+                        />
+                            
                     </div>
                 </div>
 
@@ -60,29 +123,42 @@ class Checkout extends Component {
                                 </tr>
                             </thead>
 
-                            <tbody>
-                                <tr>
-                                    <td><img src="" alt="product image"/> <Link to="#"> title </Link></td>
-                                    <td>$4561</td>
-                                    <td>x</td>
-                                    <td>2</td>
-                                    <td>$14553</td>
-                                </tr>
-                            </tbody>
-                            <tfoot>
-                                <tr>
-                                    <td>total: $56411</td> 
-                                </tr>
-                                <tr>
-                                    <td><button type="submit">Proceed to Checkout</button></td> 
-                                </tr>
-                            </tfoot>
+                                {cart.length > 0 && (
+                                    <tbody>
+                                        {cart.map((item, index, arr) => (
+                                            <tr key={index}>
+                                                <td className="thumbnail">
+                                                    <img src={item.image} alt={item.title}/> 
+                                                    <Link to="#"> {item.title} </Link>
+                                                </td>
+                                                <td>${item.price}</td>
+                                                <td>x</td>
+                                                <td>{item.count}</td>
+                                                <td>${Number(item.price*item.count)}</td>
+                                            </tr>
+                                        ))}    
+                                    </tbody>
+                                )}
                         </table>
+                        <div className="table__footer">
+                            <div>
+                            total:<span> $56411</span>
+                            </div>
+                            <div>
+                                <button type="submit">Proccess Order</button>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
          );
     }
 }
+
+const mapSateToProps = state => {
+    return {
+        cart: state.cart
+    }
+}
  
-export default Checkout;
+export default connect(mapSateToProps)(Checkout);
